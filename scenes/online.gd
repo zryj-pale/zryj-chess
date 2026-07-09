3643dc159179d453b9f742660aac5672ae7d9fdf
zryj pale <zryjpale@gmail.com>
2026-07-09 17:33:40 +0200

    odbieranie wiadomosci

--- scenes/online.gd
+++ scenes/online.gd
@@ -1,19 +1,19 @@
 extends Control
 
-var przycisk_nacisniety = true
+var przycisk_nacisniety = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$relay.connect_to_server("ws://31.70.109.158:607")
 	$relay.join_room("rapists room")
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if przycisk_nacisniety:
+		$relay.send_message($"room name".text)
 
+func przycisk_wlaczon(toggled_on: bool) -> void:
+	przycisk_nacisniety = toggled_on
 
-func _on_check_button_button_up() -> void:
-	przycisk_nacisniety = true
-
-func _on_check_button_button_down() -> void:
-	przycisk_nacisniety = false
+func _on_relay_message_received(text: String) -> void:
+	$Label.text = text
